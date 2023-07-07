8d5062960e73f150b56fb3740309de05ff0c5cf3
Sérgio Renato Steglich <79202041+srsteglich@users.noreply.github.com>
2023-07-07 19:22:03 -0300

    Alteração do exercício de While e For

--- Exercicios/exerciciocWhileFor.py
+++ Exercicios/exerciciocWhileFor.py
@@ -15,7 +15,7 @@ i = 1;
 while i <=100:
     soma = soma + i;
     i += 1;
-print(" A Soma dos números em While, de 1 à 100 é",soma);
+print(" A Soma dos números em 'While', de 1 à 100 é",soma);
 
 print("\nQuestao 4-")
 k = 100
@@ -23,7 +23,7 @@ somar = 0
 for contas in range(1,k+1):
     ##print(contas)
     somar = somar + contas    
-print(" A Soma dos números em For, de 1 à 100 é",somar)    
+print(" A Soma dos números em 'For', de 1 à 100 é",somar)    
 
 
 print("\nQuestao 5-")
@@ -40,8 +40,8 @@ print("\n Ou outro...\n")
 numpar = 1
 for p in range(1,21):
     if numpar % 2 == 0:
-        print(numpar);
-    numpar += 1;
+        print(numpar)
+    numpar += 1
 
 print("\nQuestao 7-")
 letra =  str(input("Digite uma palavra: "))
@@ -52,9 +52,27 @@ while quant >= 0:
      inver += letra[quant]
      ##print(inver)
      quant -= 1
-print(inver)
+print(inver,"\n")
+
+print("\nQuestao 8-")
+letr =  str(input("Digite uma palavra: "))
+inverti = ""
+for invert in range(len(letr)-1, -1, -1):
+    inverti += letr[invert]   
+if letr == inverti:
+    print("\n É palíndromo")         
+else:
+    print("\n NÂO É palíndromo")  # coloquei pra verificar
+print("  ")
+print(inverti)
+print(letr)
       
 print("\nQuestao 9-")
+num1 = 1
+while num1 ** 2 < 100:
+    num1 += 1
+    #print(" conta ", num1) <-- coloquei isso pra fazer teste
+print(" Menor número do quadrado cujo maior do que 1.000 é", num1)    
 
 print("\nQuestao 10-")
 hobbies  = ["surf", "natação","programar","churrasco","musculação"]
@@ -63,3 +81,6 @@ for inverto in hobbies[::-1]:
 print("\n Ou outro...\n ")
 for inverto2 in reversed(hobbies):
     print(inverto2)
+print("\n Ou outro...\n ")
+for i in range(len(hobbies)-1, -1, -1):
+    print(hobbies[i])
